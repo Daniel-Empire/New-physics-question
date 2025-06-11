9830b7d0403dca009afe4c51cb013199f750be8a
Daniel-Empire <danielboldofficial@gmail.com>
2025-06-11 14:49:03 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,13 @@
+const questions = [
+  {
+    question: "What is the unit of power?",
+    options: ["Joule", "Watt", "Newton", "Volt"],
+    answer: "Watt"
+  },
+  {
+    question: "Which instrument is used to measure electric current?",
+    options: ["Voltmeter", "Ammeter", "Galvanometer", "Ohmmeter"],
+    answer: "Ammeter"
+  }
+  // Add more questions below this line
+];
